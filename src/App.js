ba1b3b948457fbca852f06aaa2a5b448ef8102a5
Alisson-sistemas <alissonretiro@hotmail.com>
2024-01-04 14:03:33 -0300

    erro
    erro na importação

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Nome from './componts'
+function App() {
+  return(
+    <div>
+      <h1>Componentes App</h1>
+      <Nome/>
+    </div>
+  )
+}
+
+export default App;
+
+
---
+++ src/componts/Nome.js
@@ -0,0 +1,8 @@
+function Nome(){
+    return(
+      <div>
+        <h1>Components app</h1>
+      </div>
+    )
+  }
+  export default Nome
